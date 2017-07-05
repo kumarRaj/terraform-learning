689d73b42b1b5ce473e83de16569e7bdb1a60042
Raj <rajkum@thoughtworks.com>
2017-07-05 20:52:10 +0530

    Add documentation for VPC and Internet gateway

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -1,3 +1,10 @@
+/*
+This is a VPC. It's mandatory field is cidr_block.
+So the first 16 bits are fixed and the last 16 can change. Meaning 2^16 i.e. 65536 address spaces are available. See example below
+a.b.0.0 / 16  +0.0.255.255  255.255.0.0 65,536  216 256 C = B 
+
+You can't change the size of a VPC after you create it.
+*/
 resource "aws_vpc" "raj_tf" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
@@ -8,6 +15,9 @@ resource "aws_vpc" "raj_tf" {
 }
 
 
+/*
+Only one Internet gateway can be attached to a VPC at a time.
+*/
 resource "aws_internet_gateway" "raj_internet_gateway_tf" {
   vpc_id = "${aws_vpc.raj_tf.id}"
 
