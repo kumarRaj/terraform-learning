a535d145678c6d81debbef109691d11c11bbadce
Raj <rajkum@thoughtworks.com>
2017-09-21 00:02:20 +0530

    Created a public subnet. Private subnet still in progress.

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -26,7 +26,7 @@ resource "aws_internet_gateway" "raj_internet_gateway_tf" {
   }
 }
 
-resource "aws_subnet" "raj_public_subnet_tf" {
+resource "aws_subnet" "raj_public_tf" {
   vpc_id     = "${aws_vpc.raj_tf.id}"
   cidr_block = "10.0.0.0/24"
 
@@ -35,19 +35,46 @@ resource "aws_subnet" "raj_public_subnet_tf" {
   }
 }
 
+
+resource "aws_subnet" "raj_private_tf" {
+  vpc_id     = "${aws_vpc.raj_tf.id}"
+  cidr_block = "10.0.1.0/24"
+
+  tags {
+    Name = "raj_private_subnet"
+  }
+}
+
 /*
 This makes the above subnet a public subnet.
 Attaching an internet gateway to a subnet's route tables makes it a public subnet
 */
-resource "aws_route_table" "route_table_for_public_subnet" {
+resource "aws_route_table" "public_subnet" {
   vpc_id = "${aws_vpc.raj_tf.id}"
 
   route {
-    cidr_block = "10.0.0.0/24"
+    cidr_block = "0.0.0.0/0"
     gateway_id = "${aws_internet_gateway.raj_internet_gateway_tf.id}"
   }
 
   tags {
-    Name = "main"
+    Name = "raj_public_subnet_route_table"
   }
 }
+
+resource "aws_route_table_association" "public_subnet" {
+  subnet_id = "${aws_subnet.raj_public_tf.id}"
+  route_table_id = "${aws_route_table.public_subnet.id}"
+}
+
+//resource "aws_route_table" "private_subnet" {
+//  vpc_id = "${aws_vpc.raj_tf.id}"
+//  subnet_id = "${aws_subnet.raj_private_subnet_tf.id}"
+//  route {
+//    cidr_block = "0.0.0.0/0"
+//    instance_id = "${aws_instance.nat.id}"
+//  }
+//  tags {
+//    Name = "main"
+//  }
+//}
